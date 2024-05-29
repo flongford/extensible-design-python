6b6d8644658244456d3acd058c450798cf565042
flongford <flongford@enthought.com>
2024-05-29 20:59:51 +0100

    DEV: add bootstrapped sneks to server store upon start up

--- snek_lib/server/app.py
+++ snek_lib/server/app.py
@@ -6,7 +6,7 @@ from dataclasses import asdict
 from fastapi import FastAPI, Depends
 
 from snek_lib.core.i_snek_repository import ISnekRepository
-from snek_lib.utilities import load_snek_repository_factory
+from snek_lib.utilities import load_snek_repository_factory, bootstrap_sneks
 
 
 def create_app() -> FastAPI:
@@ -24,8 +24,14 @@ def create_app() -> FastAPI:
         version=importlib.metadata.version("snek_lib"),
     )
     snek_repository_factory = load_snek_repository_factory(
-        factory_name=os.getenv("SNEK_REPOSITORY_FACTORY_NAME", "fake")
+        factory_name=os.getenv("SNEK_REPOSITORY_FACTORY_NAME")
     )
+
+    # Ensure bootstrapped sneks are uploaded to data store upon
+    # deployment
+    snek_repository = snek_repository_factory()
+    for snek_id, snek in bootstrap_sneks().items():
+        snek_repository.put_snek(snek_id, snek)
 
     @app.get("/sneks")
     def get_sneks(
